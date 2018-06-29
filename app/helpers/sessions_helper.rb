03d75970bc4c3c0012f61e739015e10c55d5adcd
Matt Berg <matt@puresomni.com>
2018-06-29 11:06:35 -0400

    update users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,16 +1,21 @@
 module SessionsHelper
 
   # Logs in the given user.
-  def log_in(user)
-    session[:user_id] = user.id
-  end
+    def log_in(user)
+      session[:user_id] = user.id
+    end
 
-  # Remembers a user in a persistent session.
-  def remember(user)
-    user.remember_token
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
+    # Remembers a user in a persistent session.
+    def remember(user)
+      user.remember
+      cookies.permanent.signed[:user_id] = user.id
+      cookies.permanent[:remember_token] = user.remember_token
+    end
+
+    # Returns true if the given user is the current user.
+    def current_user?(user)
+      user == current_user
+    end
 
   # Returns the current logged-in user (if any).
   def current_user
@@ -19,7 +24,7 @@ module SessionsHelper
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
-    !current_user.nil?
+    @user == current_user
   end
 
   #forgets a persistent session
@@ -27,12 +32,23 @@ module SessionsHelper
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
-  end 
+  end
 
   # Logs out the current user.
   def log_out
-    forget(current_user)
+    current_user && forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # Redirects to stored location (or to the default).
+ def redirect_back_or(default)
+   redirect_to(session[:forwarding_url] || default)
+   session.delete(:forwarding_url)
+ end
+
+ # Stores the URL trying to be accessed.
+ def store_location
+   session[:forwarding_url] = request.original_url if request.get?
+ end
 end
