5fdef222515206874f481ab43da032c23e0510b5
Matt Berg <matt@puresomni.com>
2018-06-30 13:14:12 -0400

    tweak somethings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,7 @@ ActionMailer::Base.smtp_settings = {
   :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com',
   :enable_starttls_auto => true
+}
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
