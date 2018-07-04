6d7d35ba8da86a2ab4e87af540c10a7eb6ba9dbb
Matt Berg <matt@puresomni.com>
2018-07-04 17:58:34 -0400

    make updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,13 @@ before_action :admin_user,     only: :destroy
 
 
 def index
-  @users = User.paginate(page: params[:page])
+  @users = User.where(activated: FILL_IN).paginate(page: params[:page])
 end
 
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless FILL_IN
   end
 
   def new
@@ -20,15 +21,15 @@ end
   end
 
   def create
-   @user = User.new(user_params)
-   if @user.save
-     @user.send_activation_email
-     flash[:info] = "Please check your email to activate your account."
-     redirect_to root_url
-   else
-     render 'new'
-   end
- end
+    @user = User.new(user_params)
+    if @user.save
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
 
  def edit
    @user = User.find(params[:id])
