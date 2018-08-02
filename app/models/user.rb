8515a19f57a52e0d6ef6b2bba0529ff2c19020cd
Matt Berg <matt@puresomni.com>
2018-08-02 09:46:35 -0400

    get activation finally working

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,9 +42,10 @@ class User < ApplicationRecord
    end
 
  # Activates an account.
- def activate
-   update_columns(activated: FILL_IN, activated_at: FILL_IN)
- end
+   def activate
+      update_attribute(:activated,    true)
+      update_attribute(:activated_at, Time.zone.now)
+   end
 
  # Sends activation email.
  def send_activation_email
